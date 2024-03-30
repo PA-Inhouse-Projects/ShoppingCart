37ff97def41643d65cc10fd609288bc086749cc8
Ami Lalani <amilalani09@gmail.com>
2024-03-30 15:35:21 -0400

    changed the variable names

--- backend/data/sqliteClient.py
+++ backend/data/sqliteClient.py
@@ -27,13 +27,13 @@ class SQLClient:
         SQLClient.con.commit()
 
     def insert_table(self):
-        column1 = []
-        value1 = []
+        columns = []
+        values = []
         for key, value in self.data.get("fields").items():
-            column1.append(key)
-            value1.append(str(value))
+            columns.append(key)
+            values.append(str(value))
 
-        opr = f"INSERT INTO {self.data.get('table_name')}({', '.join(column1)}) VALUES ({', '.join(value1)});"
+        opr = f"INSERT INTO {self.data.get('table_name')}({', '.join(columns)}) VALUES ({', '.join(values)});"
         SQLClient.cur.execute(opr)
         SQLClient.con.commit()
 
